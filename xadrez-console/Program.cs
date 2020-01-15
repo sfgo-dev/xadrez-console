e0b9d7d3b3d65ae2530c252b5b28fc6e503925bd
Saulo <saulofgo@gmail.com>
2020-01-15 19:33:21 -0300

    Criando um conjunto para pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,10 +16,7 @@ namespace xadrez_console
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.Write("Origem: ");
                         Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
