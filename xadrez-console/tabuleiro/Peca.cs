3ae92a37b94180d280e0aaaa1d53cfad021316a4
Saulo <saulofgo@gmail.com>
2020-01-15 15:00:38 -0300

    Primeiros testes de movimentos das pecas

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -14,5 +14,10 @@ namespace tabuleiro
             this.tab = tab;
             this.qtdMovimentos = 0;
         }
+
+        public void incrementarQtdMovimentos()
+        {
+            qtdMovimentos++;
+        }
     }
 }
